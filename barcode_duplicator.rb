4d21513f23fe2c51c86088cb8b82e646952be059
jirkamotejl <motejl.jirka@gmail.com>
2023-11-13 09:42:47 +0100

    larger font

--- barcode_duplicator.rb
+++ barcode_duplicator.rb
@@ -55,7 +55,7 @@ class BarcodeDuplicator
           `lp -d Honeywell_3 -o position=center 'tmp/barcode.pdf'`
         else
           RQRCode::QRCode.new(@cmd).as_png.resize(200, 200).save('tmp/barcode.png')
-          `convert 'tmp/barcode.png' -background white -gravity west -extent 600x200 -fill black -pointsize 20 -annotate +200+0 "#{@cmd.upcase}" 'tmp/barcode.png'`
+          `convert 'tmp/barcode.png' -background white -gravity west -extent 600x200 -fill black -pointsize 30 -annotate +200+0 "#{@cmd.upcase}" 'tmp/barcode.png'`
           `lp -d Honeywell_3 -o scaling=100 'tmp/barcode.png'`
         end
         sleep(1)
